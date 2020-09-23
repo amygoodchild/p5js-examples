896ace14a1c4768809980a316b96754b35636e09
Amy Goodchild <amy@amygoodchild.com>
2020-09-23 13:16:45 +0100

    Add files via upload

---
+++ blank-sketch/sketch.js
@@ -0,0 +1,9 @@
+
+function setup() {
+  createCanvas(800, 600);
+  background(0,0,0);
+}
+
+function draw() {
+
+}
---
+++ first-sketch/sketch.js
@@ -0,0 +1,10 @@
+
+function setup() {
+
+  createCanvas(800, 600);
+  background(0,0,0);
+}
+
+function draw() {
+  rect(200,200,400,100);
+}
